d2eabf993c6a0d76a3426f057f94af1b6b734e8a
JESSICAELLWOOD <jessica.ellwood@menlo.edu>
2017-01-26 12:11:38 -0800

    Classifier
    Changed clf3 to QDA

--- DataAnalytics2 classifiers.py
+++ DataAnalytics2 classifiers.py
@@ -20,8 +20,7 @@ clf1 = AdaBoostClassifier()
 #2
 clf2 = KNeighborsClassifier(n_neighbors=3)
 #3
-clf3 = SVC()
-
+clf3 = QuadraticDescriminatAnalysis()
 #[height, weight, shoe_size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
      [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
