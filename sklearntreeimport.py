1f4f61ef3a2c6ab83de171f988e169f306b2cfa3
JESSICAELLWOOD <jessica.ellwood@menlo.edu>
2017-02-23 12:07:43 -0800

    edits
    added prolems blurb

--- sklearntreeimport.py
+++ sklearntreeimport.py
@@ -4,7 +4,7 @@ from sklearn import tree
 X = [[181,80,44],[177,70,43],[160,60,38],[154,54,37],[166,63,40],[190,90,47],[175,64,39],[177,70,40],[159,55,37],[171,75,42]]
 
 Y = ['m','f','f','f','m','m','m','f','m','f']
-clf = tree.DecisionTreeClassifer( )
+clf = tree.DecisionTreeClassifer()
 
 clf = clf.fit(X,Y)
 
